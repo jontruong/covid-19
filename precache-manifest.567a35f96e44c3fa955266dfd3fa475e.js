bfe32b3de24b8ddef40c16232a9fe7e698bd873b
Jonathan Truong <jjtruong90@gmail.com>
2020-04-13 22:06:28 -0700

    Updates

---
+++ precache-manifest.567a35f96e44c3fa955266dfd3fa475e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423f5e01c556f918beff7d1c99c4d95e",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "ea2909f169a0331504c4",
+    "url": "/covid-19/static/css/main.0265c98f.chunk.css"
+  },
+  {
+    "revision": "417cbfb56816806048b5",
+    "url": "/covid-19/static/js/2.469d2691.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.469d2691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2909f169a0331504c4",
+    "url": "/covid-19/static/js/main.f2145b6b.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
